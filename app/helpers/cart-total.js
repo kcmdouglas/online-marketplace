5ef2fe05fd0fb668339a49d37666107c54b860b9
Kassidy Douglas and Patrick Sullivan <kcmdouglas@gmail.com>
2016-03-03 11:48:03 -0800

    Added styles to cart page

--- app/helpers/cart-total.js
+++ app/helpers/cart-total.js
@@ -5,7 +5,16 @@ export function cartTotal(params) {
   for(var i = 0; i < params[0].length; i++) {
     total += parseInt(params[0][i].get('price'));
   }
-  return total;
+  if (total) {
+    total = total.toString().split("").reverse().join("");
+    total = total.match(/.{0,3}/g)
+    console.log(total);
+    for(var j = 0; j < total.length; j++) {
+      total[j] = total[j].split("").reverse().join("");
+    }
+    return total.reverse().join(",").replace(/^,/, '').replace(/$,/,'');
+  }
+  return 0
 }
 
 export default Ember.Helper.helper(cartTotal);
