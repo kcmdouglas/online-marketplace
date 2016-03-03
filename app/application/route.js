90e8d65694c7e2723a2f0cec29b836fe53b98f42
patsul12 <patsul12@gmail.com>
2016-03-02 20:13:00 -0800

    add user authentication using torii and torii-firebase-adapter

--- app/application/route.js
+++ app/application/route.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel() {
+    this.get('session').fetch().catch((error) => {
+      console.log(error)
+    });
+  },
+
   model() {
     return this.store.findAll('category');
   }
